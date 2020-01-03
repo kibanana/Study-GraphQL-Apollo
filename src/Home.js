e73fa1fb08f200e956dbcb2f17a1837c72eef58e
김예원 <kyw017763@gmail.com>
2020-01-03 17:35:52 +0900

    Get data and Return to page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import { HOME_PAGE } from './queries';
 
-const Home = () => "Home";
+const Home = () => (
+  <Query query={HOME_PAGE}>
+  {({loading, data, err}) => {
+    if (loading) return <span>loading</span>
+    if (err) return <span>some error occured!</span>
+    return data.movies.map((movie) => 
+      <h2 key={movie.id}>{movie.title} / {movie.rating}</h2>
+    )
+  }}
+  </Query>
+);
 
 export { Home };
